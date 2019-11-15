1ca38270f3693a205d1e33988b452d79c58d3aac
Obinna Ubah <cubah@teamapt.com>
2019-11-15 09:14:15 +0100

    Made some minor changes

--- index.js
+++ index.js
@@ -1,13 +1,33 @@
 function callAPI() {
     const model = document.getElementById('model').value;
     const text = document.getElementById('textArea').value;
-    const request ={
+    const request = {
         model: model,
         text: text,
     };
 
     console.log(request);
-
-    document.getElementById("modalLauncher").click();
+    fetchResults(request)
 }
 
+async function fetchResults(opts) {
+    response = fetch('https://flask-sentiment-analysis-app.herokuapp.com/', {
+        method: 'post',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(opts)
+    });
+
+     responseBody = await response;
+     jsonBody = await responseBody.json()
+     console.log (jsonBody);
+
+     const model = document.getElementById('genuine');
+     model.innerHTML = jsonBody.genuine
+
+     const accuracy = document.getElementById('accuracy');
+     accuracy.innerHTML = jsonBody.accuracy.toFixed(2);
+
+     document.getElementById("modalLauncher").click();
+}
