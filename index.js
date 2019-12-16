e7fa633929c7c36b1c6250c639137de21572cc95
Obinna Ubah <cubah@teamapt.com>
2019-12-16 01:45:46 +0100

    1. Remodeled pages for OptiSOurce project 2. Added sign up, login and details pages

--- index.js
+++ index.js
@@ -1,33 +1,120 @@
-function callAPI() {
-    const model = document.getElementById('model').value;
-    const text = document.getElementById('textArea').value;
+const host = 'http://localhost:4000/users';
+async function logIn() {
+    const emailAddress = document.getElementById('emailAddress').value;
+    const password = document.getElementById('password').value;
+
+    if (!emailAddress) {
+        alert("Email Address is required!");
+        return;
+    }
+    if (!password) {
+        alert("Password is required!");
+        return;
+    }
+
     const request = {
-        model: model,
-        text: text,
+        password: password,
+        emailAddress: emailAddress,
     };
 
-    console.log(request);
-    fetchResults(request)
+    logInAsync(request);
 }
 
-async function fetchResults(opts) {
-    response = fetch('https://flask-sentiment-analysis-app.herokuapp.com/', {
+async function logInAsync(opts) {
+    response = fetch(host + '/authenticate', {
         method: 'post',
         headers: {
             'Content-Type': 'application/json'
         },
         body: JSON.stringify(opts)
+    }).then(function (response) {
+        if (response.status >= 200 && response.status <= 299) {
+            return response.json();
+        } else {
+            throw Error(response.statusText);
+        }
+    }).then(function (myJson) {
+        console.log("Redirecting....");
+        window.localStorage.setItem('firstName', myJson.firstName);
+        window.localStorage.setItem('lastName', myJson.lastName);
+        window.location.replace("details.html");
+    }
+    ).catch(function (error) {
+        alert("Username or password is incorret!");
+        console.log(error);
     });
+}
 
-     responseBody = await response;
-     jsonBody = await responseBody.json()
-     console.log (jsonBody);
+function loadResults() {
+    console.log('here');
+    const firstName = window.localStorage.getItem('firstName');
+    const lastName = window.localStorage.getItem('lastName');
 
-     const model = document.getElementById('genuine');
-     model.innerHTML = jsonBody.genuine
+    console.log(typeof lastName);
 
-     const accuracy = document.getElementById('accuracy');
-     accuracy.innerHTML = jsonBody.accuracy.toFixed(2);
+    if (lastName === 'undefined' || firstName === 'undefined') {
+        alert("You must log in first!");
+        window.location.replace("login.html");
+    }
 
-     document.getElementById("modalLauncher").click();
+    console.log(firstName);
+    console.log(lastName);
+
+    const firstNameModel = document.getElementById('firstName');
+    firstNameModel.setAttribute('value', firstName);
+
+    const lastNameModel = document.getElementById('lastName');
+    lastNameModel.setAttribute('value', lastName);
+}
+
+function logOut() {
+    window.localStorage.clear();
+    alert("Logging You Out!");
+    window.location.replace("login.html");
+}
+
+function register(form) {
+    const username = form.elements.username.value;
+    const password = form.elements.password.value;
+    const emailAddress = form.elements.emailAddress.value;
+    const firstName = form.elements.firstName.value;
+    const lastName = form.elements.lastName.value;
+    const address = form.elements.address.value;
+
+    const request = {
+        "firstName": firstName,
+        "lastName": lastName,
+        "userName": username,
+        "password": password,
+        "address": address,
+        "emailAddress": emailAddress
+    };
+
+    console.log(request);
+
+    requestAsync(request);
+}
+
+async function requestAsync(opts) {
+    response = fetch(host + '/register', {
+        method: 'post',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(opts)
+    }).then(function (response) {
+        if (response.status >= 200 && response.status <= 299) {
+            return "";
+        } else {
+            throw Error(response.statusText);
+        }
+    }).then(function () {
+        alert("Registration Successful! Please Log In!")
+        console.log("Redirecting to login....");
+        window.location.replace("login.html");
+    }
+    ).catch(function (error) {
+        alert("Username already taken!");
+        console.log(error);
+    });
 }
